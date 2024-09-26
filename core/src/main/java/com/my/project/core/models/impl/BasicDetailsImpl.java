7693f4f7d6c5ea58c06422454ffa6194c191f97d
Github-Ganesh7 <saiganeshgogu@gmail.com>
2024-09-26 20:13:53 +0530

    sling model enhancement 1

--- core/src/main/java/com/my/project/core/models/impl/BasicDetailsImpl.java
+++ core/src/main/java/com/my/project/core/models/impl/BasicDetailsImpl.java
@@ -1,26 +1,32 @@
 package com.my.project.core.models.impl;
 
+
+import com.day.cq.wcm.api.Page;
 import com.my.project.core.models.BasicDetails;
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations  .injectorspecific.ValueMapValue;
 
-
-import javax.inject.Inject;
-
-@Model(adaptables = Resource.class,
+@Model(adaptables = {SlingHttpServletRequest.class},
 adapters = BasicDetails.class,
 defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class BasicDetailsImpl implements BasicDetails {
 
-    @Inject
+    @ValueMapValue
     String firstname;
 
-    @Inject
+    @ValueMapValue
     String lastname;
 
-    @Inject
+    @ValueMapValue
     String contactnumber;
+
+    @ScriptVariable
+    Page currentPage;
 
     @Override
     public String getFirstName() {
@@ -35,5 +41,10 @@ public class BasicDetailsImpl implements BasicDetails {
     @Override
     public String getContactNumber() {
         return contactnumber;
+    }
+
+    @Override
+    public String getPageTitle() {
+        return currentPage.getTitle();
     }
 }
